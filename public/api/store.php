ae6332005ba680d985d1cdffa9ee9c42f1bb04c5
Roman Onufryk <roman.onufryk@gmail.com>
2014-01-30 14:49:02 +0200

    store user

--- public/api/store.php
+++ public/api/store.php
@@ -1,7 +1,5 @@
 <?php
   $dataPath = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'users.json';
   
-  file_put_contents($dataPath, $HTTP_RAW_POST_DATA);
-  
-  header('Content-Type: application/json');
+  echo file_put_contents($dataPath, $HTTP_RAW_POST_DATA);
 ?>
